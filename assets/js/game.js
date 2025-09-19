481ab94e9d51c1b849f70b37fd2cd493d451ce88
Alexis BENOIT <alex@noesya.coop>
2025-09-19 22:27:43 +0200

    prepare game

---
+++ assets/js/game.js
@@ -0,0 +1,9 @@
+import { game } from './game/MainGame';
+import ui from './game/UI';
+import getImageExtension from './game/utils/detectImageSupport';
+import settings from './game/data/settings';
+
+( async () => {
+    // ui.setup();
+    game.setup();
+})();
